9905652a1be7d47045cec229e0e8493a5abf9291
sampanas <roman_samp@bk.ru>
2025-01-17 00:16:29 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,4 +1,25 @@
-from .core import contents, where
+"""
+Get information about what a frame is currently doing. Typical usage:
 
-__all__ = ["contents", "where"]
-__version__ = "2024.08.30"
+    import executing
+
+    node = executing.Source.executing(frame).node
+    # node will be an AST node or None
+"""
+
+from collections import namedtuple
+_VersionInfo = namedtuple('VersionInfo', ('major', 'minor', 'micro'))
+from .executing import Source, Executing, only, NotOneValueFound, cache, future_flags
+try:
+    from .version import __version__
+    if "dev" in __version__:
+        raise ValueError
+except Exception:
+    # version.py is auto-generated with the git tag when building
+    __version__ = "???"
+    __version_info__ = _VersionInfo(-1, -1, -1)
+else:
+    __version_info__ = _VersionInfo(*map(int, __version__.split('.')))
+
+
+__all__ = ["Source"]
